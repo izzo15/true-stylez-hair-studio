61960c4fb8ea46abf8a615e6df939f0c975f768d
izzo15 <aperez011599@gmail.com>
2026-05-19 16:28:50 -0400

    Add favicon to metadata icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,9 @@ export const metadata: Metadata = {
     locale: 'en_US',
     type: 'website',
   },
+  icons: {
+    icon: '/favicon.ico',
+  },
 }
 
 const seasonalStyles = getSeasonalThemeVars()
