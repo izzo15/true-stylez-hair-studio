c7bb441bac110853df57a958820d944dd686b766
izzo15 <aperez011599@gmail.com>
2026-05-20 03:45:50 -0400

    Simplify webpack config for React 18.3.1

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-const path = require('path');
-
-const reactPath = require.resolve('react');
-const reactDOMPath = require.resolve('react-dom');
-const jsxRuntimePath = require.resolve('react/jsx-runtime');
-const clientPath = require.resolve('react-dom/client');
-const serverPath = require.resolve('react-dom/server');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -14,36 +6,12 @@ const nextConfig = {
       { protocol: 'https', hostname: 'instagram.falb.fna.fbcdn.net' },
     ],
   },
-  webpack: (config, { isServer, webpack }) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'react': reactPath,
-      'react-dom': reactDOMPath,
-      'react/jsx-runtime': jsxRuntimePath,
-      'react-dom/client': clientPath,
-      'react-dom/server': serverPath,
-    };
-    
-    config.plugins.push(
-      new webpack.NormalModuleReplacementPlugin(
-        /^react-dom\/client$/,
-        clientPath
-      ),
-      new webpack.NormalModuleReplacementPlugin(
-        /^react\/jsx-runtime$/,
-        jsxRuntimePath
-      )
-    );
-    
-    return config;
-  },
   experimental: {
     serverActions: {
       allowedOrigins: ['localhost:3000', 'true-stylez-hair-studio.vercel.app']
     }
   },
   trailingSlash: false,
-  transpilePackages: ['@react-three/drei', '@react-three/fiber', 'next-auth'],
-};
+}
 
 module.exports = nextConfig;
